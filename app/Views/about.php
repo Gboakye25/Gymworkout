029005709b04fd2751475849eec57f61c06c7a0d
Godfred Boakye <G.boakye@wlv.ac.uk>
2026-03-26 17:32:47 +0000

    Add about page content and website features section

--- app/Views/about.php
+++ app/Views/about.php
@@ -1,6 +1,32 @@
 <?= $this->include('layouts/header') ?>
 
-<h1>About</h1>
-<p>This website helps you find gym workouts.</p>
+<div class="row">
+    <div class="col-md-8">
+        <h1 class="mb-4">About FitTrack</h1>
+        <p>
+            FitTrack is a simple gym workouts website created to help users find useful workout ideas for different fitness goals.
+        </p>
+        <p>
+            The website is designed for people who want an easy way to view workout types such as beginner training, strength workouts, lower body sessions, and cardio routines.
+        </p>
+        <p>
+            This project was developed using CodeIgniter, PHP, HTML, and CSS as part of a university web development assignment.
+        </p>
+    </div>
+
+    <div class="col-md-4">
+        <div class="card shadow-sm">
+            <div class="card-body">
+                <h4>Website Features</h4>
+                <ul>
+                    <li>Simple gym workout pages</li>
+                    <li>Easy navigation</li>
+                    <li>Clean design</li>
+                    <li>Dynamic features to be added</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?= $this->include('layouts/footer') ?>
